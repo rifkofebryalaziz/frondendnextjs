424a04ec95621ffbe5f63dfc74cd84516270717f
rifkofebryalaziz <rifkofebryalaziz30@gmail.com>
2025-09-01 15:13:45 +0700

    2

--- src/app/sample-segments/page.tsx
+++ src/app/sample-segments/page.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 
-type Props = { params: Promise<{ segments: string }>};
+type Props = { params: Promise<{ segments: string[] }> };
 
 const page = async (props: Props) => {
-    const segments = (await props.params).segments;
+  const segments = (await props.params).segments;
 
-  return(
-  <main>
-    <p>segments 1:{segments[0]}</p>
-    <p>segments 2:{segments[1]}</p>
-  </main>
+  return (
+    <main>
+      <p>segments 1: {segments[0]}</p>
+      <p>segments 2: {segments[1]}</p>
+    </main>
   );
 };
 
-export default page
+export default page;
